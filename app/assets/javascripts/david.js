6cca544004b33a27b0bad9c37a24a2cbb9143b48
David Příhoda <prihoda1@fit.cvut.cz>
2015-03-21 17:07:00 +0100

    map with timeline

--- app/assets/javascripts/david.js
+++ app/assets/javascripts/david.js
@@ -1,6 +1,7 @@
-(function(){
+(function () {
 
-    var width, height, parseDate, formatPercent, x, y, colors, xAxis, area, stack, svg;
+    var width, height, parseDate, formatPercent, x, y, colors, xAxis, area, stack, svg, bgPath, fgPath, addedAxis;
+    var plotData = null;
     $.plotInit = function () {
 
         var margin = {top: 20, right: 20, bottom: 30, left: 0},
@@ -19,53 +20,94 @@
             .orient("bottom");
 
         area = d3.svg.area()
-            .x(function(d) { return x(d.date); })
+            .x(function (d) {
+                return x(d.date);
+            })
             .y0(height)
-            .y1(function(d) { return y(d.value); });
+            .y1(function (d) {
+                return y(d.value);
+            });
 
         svg = d3.select("#timeline").append("svg")
             .attr("width", width + margin.left + margin.right)
             .attr("height", height + margin.top + margin.bottom)
             .append("g")
             .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        bgPath = svg.append("path");
+        fgPath = svg.append('path');
 
-        $.plotUpdate(null, {
-            max : 123,
-            values: [{
-                date:'2015-03-21T07:20:30.478+01:00', value:100},
-                {date:'2015-03-21T07:20:31.633+01:00', value:50},
-                {date:'2015-03-21T07:20:32.633+01:00', value:10},
-                {date:'2015-03-21T07:20:38.633+01:00', value:30}]
-        });
+        plotData = [
+            {
+                id: 10,
+                max: 123,
+                values: [{
+                    date: '2015-03-21T07:20:30.478+01:00', value: 100
+                },
+                    {date: '2015-03-21T07:20:31.633+01:00', value: 50},
+                    {date: '2015-03-21T07:20:32.633+01:00', value: 10},
+                    {date: '2015-03-21T07:20:38.633+01:00', value: 30}]
+            }, {
+                id: 10,
+                max: 95,
+                values: [{
+                    date: '2015-03-21T07:20:30.478+01:00', value: 50
+                },
+                    {date: '2015-03-21T07:20:31.633+01:00', value: 70},
+                    {date: '2015-03-21T07:20:32.633+01:00', value: 60},
+                    {date: '2015-03-21T07:20:38.633+01:00', value: 90}]
+            }, {
+                id: 'all',
+                max: 95,
+                values: [{
+                    date: '2015-03-21T07:20:30.478+01:00', value: 50
+                },
+                    {date: '2015-03-21T08:20:31.633+01:00', value: 70},
+                    {date: '2015-03-21T12:20:32.633+01:00', value: 60},
+                    {date: '2015-03-21T19:20:38.633+01:00', value: 90}]
+            }
+        ];
 
-    }
-    $.plotUpdate = function (error, data) {
-        var allMax = [];
-        data.values.forEach(function(d) {
-            d.date = parseDate(d.date);
-            allMax.push({date: d.date, value:data.max});
-        });
-
-        x.domain(d3.extent(data.values, function(d) { return d.date; }));
-        y.domain([0, data.max]);
-
-        console.log(data.values);
-        svg.append("path")
-            .datum(allMax)
-            .attr("class", "timeline-bg")
-            .attr("d", area).attr('fill','#dd0000')
-            .on('click', function(){
-                console.log('naow');
-            })
-        svg.append("path")
-            .datum(data.values)
-            .attr("class", "timeline-area")
-            .attr("d", area).attr('fill','#00ff00');
-
-        svg.append("g")
+        addedAxis = svg.append("g")
             .attr("class", "x axis")
             .attr("transform", "translate(0," + height + ")")
-            .call(xAxis);
+
+        $.plotUpdate('all');
+    }
+    $.plotUpdate = function (nodeId) {
+        var allMax = [];
+        var data = [];
+        var max = 0;
+
+        plotData.forEach(function (d) {
+            if (d.id != nodeId) return;
+            max = d.max;
+            d.values.forEach(function (d) {
+                if (typeof d.date == 'string')
+                    d.date = parseDate(d.date);
+                allMax.push({date: d.date, value: max});
+                data.push({date: d.date, value: d.value});
+            });
+        });
+
+        x.domain(d3.extent(data, function (d) {
+            return d.date;
+        }));
+        y.domain([0, max]);
+
+        console.log(data);
+        bgPath
+            .datum(allMax)
+            .attr("class", "timeline-bg")
+            .attr("d", area).attr('fill', '#dd0000')
+            .on('click', function () {
+                console.log('naow');
+            })
+        fgPath
+            .datum(data)
+            .attr("class", "timeline-area")
+            .attr("d", area).attr('fill', '#00ff00');
+
+        addedAxis.call(xAxis);
 
     }
 
