e061bad59f194d2cb6779ca382507bcca44c4006
Tomas Barton <barton.tomas@gmail.com>
2015-03-21 17:28:53 +0100

    traffic

--- app/api/hckv1/nodes.rb
+++ app/api/hckv1/nodes.rb
@@ -33,5 +33,26 @@ module Hckv1
       client.actors
     end
 
+    resource 'traffic' do
+      params do
+        optional :limit, default: 20, type: Integer
+        optional :offset, default: 0, type: Integer
+        optional :page, default: 0, type: Integer
+      end
+      get '/' do
+        # for simple pagination
+        if params[:page] > 0
+          per_page = params[:limit]
+          params[:offset] = params[:page] * per_page
+        end
+        res = Event.all.sort(limit: [params[:offset], params[:limit]])
+           #  .order(id: :asc)
+        res.map do |e|
+          {event_id: e.event_id, happened_at: e.happened_at}
+        end
+      end
+    end
+
+
   end
 end
