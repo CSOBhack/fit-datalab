22fbaaefb2a27f3fefb85d558709201328cf46d1
David Příhoda <prihoda1@fit.cvut.cz>
2015-03-21 14:33:58 +0100

    D3 czech map

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,63 +1,67 @@
+(function () {
+    var $;
 
-(function() {
-  var $;
+    $ = jQuery;
 
-  $ = jQuery;
+    $.mapInit = function () {
+        var width = 800,
+            height = 400;
 
- $.mapInit = function () {
-     var width = 800,
-         height = 400;
+        var projection = d3.geo.mercator()
+            .translate([-700*1.5, 2700*1.5])
+            .scale(width / 2 / Math.PI * 22*1.5);
 
-     var projection = d3.geo.mercator()
-         .translate([0, 0])
-         .scale(width / 2 / Math.PI);
+        var zoom = d3.behavior.zoom()
+            .scaleExtent([1, 50])
+            .scale(1.5)
+            .on("zoom", move);
 
-     var zoom = d3.behavior.zoom()
-         .scaleExtent([1, 50]).scale(2)
-         .on("zoom", move);
+        var path = d3.geo.path()
+            .projection(projection);
 
-     var path = d3.geo.path()
-         .projection(projection);
+        var svg = d3.select("#map").attr('style','width: '+width+'px; height: '+height+'px').append("svg")
+            .attr("width", width)
+            .attr("height", height)
+            .append("g")
+            .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
+            .call(zoom);
 
-     var svg = d3.select("#map").append("svg")
-         .attr("width", width)
-         .attr("height", height)
-         .append("g")
-         .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
-         .call(zoom);
+        var g = svg.append("g");
+        var czk = g.append('g')
+            .attr("transform", "translate(-34.5,128)");
+        svg.append("rect")
+            .attr("class", "overlay")
+            .attr("x", -width / 2)
+            .attr("y", -height / 2)
+            .attr("width", width)
+            .attr("height", height);
+        svg.call(zoom.event);
+        d3.json("/assets/map.json", function (error, map) {
+            if (error) return console.error(error);
+            czk.append("path")
+                .datum(topojson.feature(map, map.objects.cze)).attr('class', 'land')
+                .attr("d", path);
 
-     var g = svg.append("g");
-     var czk = g.append('g')
-         .attr("transform", "translate(-211,118)");
-     svg.append("rect")
-         .attr("class", "overlay")
-         .attr("x", -width / 2)
-         .attr("y", -height / 2)
-         .attr("width", width)
-         .attr("height", height);
-
-     d3.json("/assets/map.json", function(error, map) {
-         if (error) return console.error(error);
-         czk.append("path")
-             .datum(topojson.feature(map, map.objects.cze)).attr('class','land')
-             .attr("d", path);
-
-     });
-
-     function move() {
-         g.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
-     }
-
-     $.ajax({
-        'url':'/api/v1/nodes'
-    }).success(function(nodes){
-        $.each(nodes, function(i,node){
-            addNodeMarker(node, map);
+            $.ajax({
+                'url': '/api/v1/nodes'
+            }).success(function (nodes) {
+                czk
+                    .selectAll('circle')
+                    .data(nodes)
+                    .enter()
+                    .append('circle')
+                    .attr("cx", function (n) { var d = [n.venue_long, n.venue_lat]; return projection(d)[0]; })
+                    .attr("cy", function (n) { var d = [n.venue_long, n.venue_lat]; return projection(d)[1]; })
+                    .attr("r", "3px")
+                    .attr("fill", "red")
+            });
         });
-    });
-}
 
-function addNodeMarker(node, map){
+        function move() {
+            g.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
+        }
 
-}
+
+    }
+
 }).call(this);
