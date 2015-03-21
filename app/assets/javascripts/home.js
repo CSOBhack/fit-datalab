1dd25035a3640e9d06655bcb61b836514ebef86b
David Příhoda <prihoda1@fit.cvut.cz>
2015-03-21 13:53:46 +0100

    D3 czech map

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -4,14 +4,51 @@
 
   $ = jQuery;
 
- $.mapInit = function initialize() {
+ $.mapInit = function () {
+     var width = 800,
+         height = 400;
 
-  var mapOptions = {
-      center: new google.maps.LatLng(50.0018876,15.6641636),
-      zoom: 7
-  };
-    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-    $.ajax({
+     var projection = d3.geo.mercator()
+         .translate([0, 0])
+         .scale(width / 2 / Math.PI);
+
+     var zoom = d3.behavior.zoom()
+         .scaleExtent([1, 50]).scale(2)
+         .on("zoom", move);
+
+     var path = d3.geo.path()
+         .projection(projection);
+
+     var svg = d3.select("#map").append("svg")
+         .attr("width", width)
+         .attr("height", height)
+         .append("g")
+         .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
+         .call(zoom);
+
+     var g = svg.append("g");
+     var czk = g.append('g')
+         .attr("transform", "translate(-211,118)");
+     svg.append("rect")
+         .attr("class", "overlay")
+         .attr("x", -width / 2)
+         .attr("y", -height / 2)
+         .attr("width", width)
+         .attr("height", height);
+
+     d3.json("/assets/map.json", function(error, map) {
+         if (error) return console.error(error);
+         czk.append("path")
+             .datum(topojson.feature(map, map.objects.cze)).attr('class','land')
+             .attr("d", path);
+
+     });
+
+     function move() {
+         g.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
+     }
+
+     $.ajax({
         'url':'/api/v1/nodes'
     }).success(function(nodes){
         $.each(nodes, function(i,node){
@@ -21,29 +58,6 @@
 }
 
 function addNodeMarker(node, map){
-    //console.log('Adding',node)
-    var myLatLng = new google.maps.LatLng(node.venue_lat, node.venue_long);
-    var marker = new google.maps.Marker({
-        position: myLatLng,
-        map: map,
-        title: node.venue_name
-    });
-    var contentString = '<div id="content">'+
-        '<div id="siteNotice">'+
-        '</div>'+
-        '<h3 id="firstHeading" class="firstHeading">'+node.venue_name+'</h3>'+
-        '<h4 id="secondHeading" class="secondHeading">IP '+node.ip_address+'</h4>'+
-        '<div id="bodyContent">'+
-        '<p></p>'+
-        '</div>'+
-        '</div>';
-
-    var infowindow = new google.maps.InfoWindow({
-        content: contentString
-    });
-    google.maps.event.addListener(marker, 'click', function() {
-        infowindow.open(map,marker);
-    });
 
 }
 }).call(this);
---
+++ app/assets/javascripts/map.js
@@ -0,0 +1,7 @@
+/**
+ * Created by david on 21.3.15.
+ */
+(function(){
+
+
+}).call(this);
